7b44a1840ee194d5adb3ded2e038bdda513b3c6a
UiRobotics <31750785+UiRobotics@users.noreply.github.com>
2018-09-30 20:22:08 +0000

    Add files via upload
    added terminating character buffer

--- slave/coms/recieveData.py
+++ slave/coms/recieveData.py
@@ -12,7 +12,14 @@ client = pyfldigi.Client()
 
 # takes in messages recieved by fldigi, strips them of white space, and
 # puts them as a string in curr_message
+buffer = ''
 while True:
-    curr_message = client.text.get_rx_data().decode('UTF-8').strip()
-    print(curr_message)
-    time.sleep(2.5)
+    curr_message = client.text.get_rx_data().decode('UTF-8')
+    if len(curr_message) != 0:
+        buffer += curr_message
+    # uses 001 as a terminating character and prints only when 001 is recieved
+    if "001" in buffer:
+        print(buffer)
+        buffer = ''
+
+    time.sleep(1)

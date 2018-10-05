65f3f6d3f1f4dfbfe7691ec25e5573b95264a3a6
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2018-10-04 20:30:29 -0500

    Add files via upload

--- slave/coms/recieveData.py
+++ slave/coms/recieveData.py
@@ -12,31 +12,38 @@ client = pyfldigi.Client()
 client.modem.id = 42        # sets op mode
 client.modem.carrier = 2500 # sets cursor frequency
 
+startString = "IDevice:"
+endString = ";"
+
+def parseM(message):
+    message = message[len(startString):-len(endString)] # removes the starting and terminating characters
+    # continue parsing, use command-value (maybe list of touples), will eventually go to ros topics or something
+
+
 def expectMessage():        # takes in messages recieved by fldigi, strips them of white space, and prints until 'REPLY' is sent
     RECIEVING = True
     buffer = ''
-    startString = "IDevice"
-    endString = ";"
     while RECIEVING:
         curr_buffer = client.text.get_rx_data().decode('UTF-8').strip()
         if len(curr_buffer) != 0:
             buffer += curr_buffer
-        if endString in buffer: #only looks for messages in buffer if there's a terminating character in the buffer
+        if endString in buffer: # only looks for messages in buffer if there's a terminating character in the buffer
             start = buffer.find(startString)
             end = buffer.find(endString) + len(endString) -1
             if not start == -1:
                 message = buffer[start:end]
                 print(message)
+                parseM(message)
                 buffer = ''
 
         if "REPLY" in buffer:
             RECIEVING = False
 
-        #time.sleep(1) #try not having a delay
+        time.sleep(1) #try not having a delay
 
 def sendData():
     client.main.send("TODO: actually send data")
 
-while True:
+'''while True:
     expectMessage()
-    sendData()
+    sendData()'''

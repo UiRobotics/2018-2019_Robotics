8fd80a9847e7ed4d8fa6617392ab70f4739dd22f
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2019-08-10 21:38:36 -0500

    Add files via upload

---
+++ slave/ardo/compSerialRead.ino
@@ -0,0 +1,15 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available()) {
+    String input = Serial.readString();
+    input = input.substring(1);
+    Serial.println(input);
+    int data[6] = {0,0,0,0,0,0};
+    for (int i = 0; i < 6; i++) {
+      data[i] = input.substring(i*3,(i*3)+3).toInt();
+    }
+  }
+}

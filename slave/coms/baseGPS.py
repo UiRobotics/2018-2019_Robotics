0432c4e51be299486284eea51fcb5f906da93105
Kefka174 <31826754+Kefka174@users.noreply.github.com>
2019-08-11 21:33:43 -0500

    Add files via upload

--- slave/coms/baseGPS.py
+++ slave/coms/baseGPS.py
@@ -29,39 +29,6 @@ endString = ";"
     restartFldigi() - restarts the app and client
 '''
 
-def safeSendCommand(command):
-    checker = hashlib.md5()                                               #
-    checker.update(command.encode())                                      # calculating the md5sum of the command
-    calculatedSum = (b'%02X' % (sum(checker.digest()) & 0xFF)).decode()   #
-    timeOut = 10 + len(command)
-    stringToSend = startString + calculatedSum + ':' + command + endString
-    client.main.send(stringToSend, timeout=timeOut)
-    while not awaitFailure():
-        print('Transmition failed - Resending . . .')
-        client.main.send(stringToSend, timeout=timeOut)
-    print('Good to go on another one cheif')
-
-def fastSendCommand(command):
-    timeOut = 10 + len(command)
-    stringToSend = startString + 'XX' + ':' + command + endString
-    client.main.send(stringToSend, timeout=timeOut)
-    print('Good to go on another one cheif')
-
-def awaitFailure():
-    t_end = time.time() + 10
-    noResponse = True
-    buffer = ''
-    while (time.time() < t_end) and noResponse:
-        curr_buffer = client.text.get_rx_data().decode('UTF-8')
-        if len(curr_buffer) != 0:
-            buffer += curr_buffer
-            if 'BAD' in buffer:
-                noResponse = False
-                return False
-            elif 'GOOD' in buffer:
-                noResponse = False
-                return True
-
 def yellCallsign():
     client.main.send("de" + (" " + CALL_SIGN)*3 + " k\n", timeout=15)
     client.delay(1500)
--- slave/coms/sendGPS.py
+++ slave/coms/sendGPS.py
@@ -28,57 +28,18 @@ endString = ";"
     restartFldigi() - restarts the app and client
 '''
 
-gpsSendTime = time.clock() - 10
+gpsSendTime = time.clock() - 5
 
 def main():
     global gpsSendTime
-    buffer = ''
     while True:
-        curr_buffer = client.text.get_rx_data().decode('UTF-8')
-        if len(curr_buffer) != 0:
-            buffer += curr_buffer
-        elif (time.clock() - gpsSendTime) > 15:
-            gpsSendTime = time.clock()
-            gps()
-        if endString in buffer: # only looks for messages in buffer if there's a terminating character in the buffer
-            if startString in buffer:
-                start = buffer.find(startString)
-                end = buffer.find(endString) + len(endString)
-                message = buffer[start:end]
-                print("message recieved = " + message)#####################################################
-                parseM(message)
-                buffer = ''
-            else: # if theres a terminating character but no start string
-                time.sleep(4)
-                client.main.send("de BAD k\n", timeout=15)
-                buffer = ''
-        time.sleep(1)
-
-def rosPost(command):
-    try:
-        exec(command)
-    except: pass
-
-def parseM(message):
-    message = message[len(startString):] # removes the starting characters
-    sentSum = message[:2]                # gather checksum
-    message = message[3:]                # remove checksum from message
-    valueEnd = message.find(endString)   # 
-    command = message[:valueEnd]         # remove the terminating character from the string
-    
-    checker = hashlib.md5()                                               #
-    checker.update(command.encode())                                      # calculating the md5sum of the command
-    calculatedSum = (b'%02X' % (sum(checker.digest()) & 0xFF)).decode()   #
-
-    if sentSum == 'XX':
-        rosPost(command)
-    elif sentSum == calculatedSum:
-        time.sleep(4)
-        client.main.send("de GOOD k\n", timeout=15)
-        rosPost(command)
-    else:
-        time.sleep(4)
-        client.main.send("de BAD k\n", timeout=15)
+        try:
+            if (time.clock() - gpsSendTime) > 10:
+                gpsSendTime = time.clock()
+                gps()
+            time.sleep(1)
+        except:
+            restartFldigi()
 
 def restartFldigi():
     global app
@@ -89,19 +50,16 @@ def restartFldigi():
     client = pyfldigi.Client()
     client.modem.carrier = 2500
     time.sleep(2)
-    client.main.send("de restart successful k/n", timeout=15)
 
 def changeFrequency(freq):
     client.rig.frequency = freq * 1000
     client.modem.carrier = 2500
     time.sleep(1)
-    client.main.send("de frequency set to " + str(client.rig.frequency) + " k/n", timeout = 15)
 
 def opMode(modeName):
     client.modem.name = modeName
     client.modem.carrier = 2500
     time.sleep(1)
-    client.main.send("de opMode set at " + client.modem.name + " k/n", timeout=15)
 
 def gps():
     data = arduino.readline()[:-2] #the last bit gets rid of the new-line chars
